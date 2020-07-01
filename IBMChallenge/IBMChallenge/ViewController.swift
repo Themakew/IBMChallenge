74e71550bea69d07ac32649c24a19747a0ec5564
Ruyther Costa <ruyther.costa@foton.la>
2020-07-01 11:06:21 -0300

    Initial commit

---
+++ IBMChallenge/IBMChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBMChallenge
+//
+//  Created by Foton on 01/07/20.
+//  Copyright © 2020 ruyther. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
